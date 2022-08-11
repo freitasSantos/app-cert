ab85515551d39f7c7d55d6d17855d2f923bc480a
freitasSantos <rrafaelfreitas@hotmail.com>
2022-08-10 23:05:28 -0300

    Configuração do seeders sequelize

---
+++ api/database/seeders/20220811010239-demo-certificate.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+     await queryInterface.bulkInsert('Certificates', [{
+      
+    ambiente:"HML",
+    origem:"Interno",
+    proprietario:"Rodrigo S.A.",
+    proprietarioNome:"",
+    certificado:"SSL",
+    funcionalidade:"Portal Web Site",
+    tipo:"ICP - Brasil",
+    emissao:"2022-08-10",
+    validade:"2023-08-09",
+    createdAt: new Date(),
+    updatedAt: new Date()
+      }], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+     await queryInterface.bulkDelete('Certificates', null, {});
+     
+  }
+};
